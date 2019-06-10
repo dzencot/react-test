53285d6d146492689e2f3c5177957197c3fc6df7
Ivan Gagarinov <dzencot@gmail.com>
2019-06-10 08:33:21 +0500

    fixes

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -5,23 +5,33 @@ import * as actions from '../actions';
 import { config } from '../config';
 
 const basket = handleActions({
-  [actions.addItem](state, payload) {
-    const { item, countItem } = payload.payload;
+  [actions.addItem](state, { payload }) {
+    const { item } = payload;
+    const { countItem } = payload;
     const { items } = state;
     const oldCountItem = _.get(items, `${item.id}.countItem`, 0);
-    const newCount = _.parseInt(oldCountItem) + _.parseInt(countItem);
+    let newCount = _.parseInt(oldCountItem) + _.parseInt(countItem);
+    // TODO: сделать вывод ошибок
+    if (newCount <= 0) {
+      return state;
+    }
+    if (newCount > _.parseInt(item.quantity)) {
+      newCount = item.quantity;
+    }
     const newItems = { ...items, [item.id]: { item, countItem: newCount } };
     const newState = { ...state, items: newItems };
     const dataSessionStorage = JSON.stringify({ items: newItems });
     sessionStorage.setItem(config.appCode, dataSessionStorage);
     return newState;
   },
-  [actions.itemCountChange](state, payload) {
-    const { item } = payload.payload;
-    let { countItem } = payload.payload;
-    // TODO: сделать проверку наличия количества товаров
-    if (countItem < 0) {
+  [actions.itemCountChange](state, { payload }) {
+    const { item } = payload;
+    let { countItem } = payload;
+    if (_.parseInt(countItem) < 0) {
       countItem = 0;
+    }
+    if (_.parseInt(countItem) > _.parseInt(item.quantity)) {
+      countItem = item.quantity;
     }
     const { items } = state;
     const newItems = { ...items, [item.id]: { item, countItem } };
@@ -30,17 +40,19 @@ const basket = handleActions({
     sessionStorage.setItem(config.appCode, dataSessionStorage);
     return newState;
   },
-  [actions.incrementItem](state, payload) {
-    // TODO: добавить проверку наличия
-    const payloadItem = payload.payload;
+  [actions.incrementItem](state, { payload }) {
+    const payloadItem = payload;
     const { items } = state;
     const item = _.get(items, payloadItem.id);
-    const newCount = (_.parseInt(item.countItem) || 0) + 1;
+    let newCount = (_.parseInt(item.countItem) || 0) + 1;
+    if (newCount > _.parseInt(item.item.quantity)) {
+      newCount = item.item.quantity;
+    }
     item.countItem = newCount;
     return { ...state, items };
   },
-  [actions.decrementItem](state, payload) {
-    const payloadItem = payload.payload;
+  [actions.decrementItem](state, { payload }) {
+    const payloadItem = payload;
     const { items } = state;
     const item = _.get(items, payloadItem.id);
     const newCount = (_.parseInt(item.countItem) || 0) - 1;
@@ -49,8 +61,8 @@ const basket = handleActions({
     }
     return { ...state, items };
   },
-  [actions.removeItem](state, payload) {
-    const payloadItem = payload.payload;
+  [actions.removeItem](state, { payload }) {
+    const payloadItem = payload;
     const { items } = state;
     const newItems = _.omit(items, payloadItem.id);
     return { ...state, items: newItems };
@@ -62,23 +74,23 @@ const basket = handleActions({
 }, '');
 
 const appState = handleActions({
-  [actions.selectCategory](state, payload ) {
-    const category = payload.payload;
+  [actions.selectCategory](state, { payload } ) {
+    const category = payload;
     return { ...state, currentView: 'category', category };
   },
-  [actions.selectItem](state, payload) {
-    const item = payload.payload;
+  [actions.selectItem](state, { payload }) {
+    const item = payload;
     return { ...state, currentView: 'item', item };
   },
-  [actions.returnHome](state, { payload }) {
+  [actions.returnHome](state) {
     return { ...state,  category: false, item: false, currentView: 'categories' };
   },
-  [actions.loadCategories](state, payload) {
-    const categories = payload.payload;
+  [actions.loadCategories](state, { payload }) {
+    const categories = payload;
     return { ...state, categories };
   },
-  [actions.loadItems](state, payload) {
-    const items = payload.payload;
+  [actions.loadItems](state, { payload }) {
+    const items = payload;
     return { ...state, items };
   },
 }, '');
